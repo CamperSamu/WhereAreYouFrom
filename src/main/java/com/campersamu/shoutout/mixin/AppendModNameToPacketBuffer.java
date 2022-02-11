01c06ccc0cbd3e2852527e17e7cbe7ba91433f88
Camper_Samu <samueleverga@gmail.com>
2022-02-11 16:59:41 +0100

    fix: WYM I DIDN'T TESTED CUSTOM LORE

--- src/main/java/com/campersamu/shoutout/mixin/AppendModNameToPacketBuffer.java
+++ src/main/java/com/campersamu/shoutout/mixin/AppendModNameToPacketBuffer.java
@@ -3,24 +3,20 @@ package com.campersamu.shoutout.mixin;
 import com.campersamu.shoutout.duck.OriginalItemDuck;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
-import net.minecraft.client.item.TooltipContext;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.nbt.NbtString;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.text.LiteralText;
-import net.minecraft.text.MutableText;
-import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.registry.Registry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-import java.util.ArrayList;
 import java.util.Optional;
 
 import static net.minecraft.item.ItemStack.DISPLAY_KEY;
@@ -35,24 +31,13 @@ public class AppendModNameToPacketBuffer {
     }
 
     private ItemStack appendModName(ItemStack itemStack) {
-        var tooltips = new ArrayList<Text>(0);
         var ogStack = ((OriginalItemDuck) this).getOgItemStack();
         var item = ogStack.getItem();
-        try {
-            item.appendTooltip(ogStack, itemStack.getHolder() instanceof PlayerEntity player ? player.world : null, tooltips, TooltipContext.Default.NORMAL);
-        } catch (Exception ignored) {
-        }
-
-        NbtList list = new NbtList();
-        for (Text line : tooltips) {
-            if (line instanceof MutableText mt)
-                list.add(NbtString.of(toJson(mt.setStyle(mt.getStyle().withItalic(false)))));
-        }
-
-        String modName = getModName(item);
-        list.add(NbtString.of(toJson(new LiteralText(modName).formatted(Formatting.BLUE, Formatting.ITALIC))));
 
         NbtCompound display = itemStack.getOrCreateSubNbt(DISPLAY_KEY);
+        NbtList list = display.contains(LORE_KEY, NbtElement.LIST_TYPE) ? display.getList(LORE_KEY, NbtElement.STRING_TYPE) : new NbtList();
+        list.add(NbtString.of(toJson(new LiteralText(getModName(item)).formatted(Formatting.BLUE, Formatting.ITALIC))));
+
         display.put(LORE_KEY, list);
         return itemStack;
     }
