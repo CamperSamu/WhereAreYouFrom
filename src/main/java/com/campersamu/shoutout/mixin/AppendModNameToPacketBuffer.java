36df779e4e1747edad0cfa16d2fa33efc83589c7
Camper_Samu <samueleverga@gmail.com>
2022-09-27 10:29:59 +0200

    Attempt at fixing mod name leaking into the real item because of other mods

--- src/main/java/com/campersamu/shoutout/mixin/AppendModNameToPacketBuffer.java
+++ src/main/java/com/campersamu/shoutout/mixin/AppendModNameToPacketBuffer.java
@@ -19,30 +19,38 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import java.util.Optional;
 
+import static java.util.Objects.requireNonNull;
 import static net.minecraft.item.ItemStack.DISPLAY_KEY;
 import static net.minecraft.item.ItemStack.LORE_KEY;
-import static net.minecraft.nbt.NbtElement.*;
+import static net.minecraft.nbt.NbtElement.LIST_TYPE;
+import static net.minecraft.nbt.NbtElement.STRING_TYPE;
 import static net.minecraft.text.Text.Serializer.toJson;
 import static net.minecraft.text.Text.literal;
 
 @Mixin(value = PacketByteBuf.class, priority = 5000)
 public class AppendModNameToPacketBuffer {
-        @ModifyVariable(method = "writeItemStack(Lnet/minecraft/item/ItemStack;)Lnet/minecraft/network/PacketByteBuf;", at = @At("HEAD"), argsOnly = true)
-        private ItemStack alterLore(ItemStack inStack) {
-            if (!inStack.isEmpty()) {
-                return appendModName(inStack.copy());   //We have to copy the ItemStack, otherwise it will cause a Concurrent Modification Exception with more than one player
-            }
-            return inStack;
+    @ModifyVariable(method = "writeItemStack(Lnet/minecraft/item/ItemStack;)Lnet/minecraft/network/PacketByteBuf;", at = @At("HEAD"), argsOnly = true)
+    private ItemStack alterLore(ItemStack inStack) {
+        if (inStack.getNbt() != null && inStack.getSubNbt(DISPLAY_KEY) != null){
+            try {   //attempt to fix the real item lore alteration caused by other mods
+                final var list = requireNonNull(inStack.getSubNbt(DISPLAY_KEY)).getList(LORE_KEY, LIST_TYPE);
+                final var modText = NbtString.of(toJson(literal(getModName(inStack.getItem())).formatted(Formatting.BLUE, Formatting.ITALIC)));
+                if (list != null){
+                    list.remove(modText);
+                }
+            } catch (NullPointerException ignored){}
         }
+        return appendModName(inStack.copy());   // We have to copy the ItemStack, otherwise it will cause a Concurrent Modification Exception with more than one player
+                                                // also this will apply the lore to a fake item to send to the client instead of modifying the real one
+    }
 
     @Contract("_ -> param1")
     private @NotNull ItemStack appendModName(@NotNull ItemStack itemStack) {
-        var ogStack = ((OriginalItemDuck) this).getOgItemStack();
-        var item = ogStack.getItem();
+        final var ogStack = ((OriginalItemDuck) this).getOgItemStack();
+        final var item = ogStack.getItem();
 
         NbtCompound display = itemStack.getOrCreateNbt().getCompound(DISPLAY_KEY);
         NbtList list = display.getList(LORE_KEY, STRING_TYPE);
-
 
         NbtString modText = NbtString.of(toJson(literal(getModName(item)).formatted(Formatting.BLUE, Formatting.ITALIC)));
 
