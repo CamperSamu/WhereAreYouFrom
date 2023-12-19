7432efbf62f92a97298a88c0519e0e638054ada3
Camper_Samu <samueleverga@gmail.com>
2023-12-19 22:35:22 +0100

    feat: Update to 1.20.4

--- src/main/java/com/campersamu/shoutout/Init.java
+++ src/main/java/com/campersamu/shoutout/Init.java
@@ -1,8 +1,15 @@
 package com.campersamu.shoutout;
 
 import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
 
 public class Init implements DedicatedServerModInitializer {
 	// This logger is used to write text to the console and the log file.
@@ -13,5 +20,16 @@ public class Init implements DedicatedServerModInitializer {
 	@Override
 	public void onInitializeServer() {
 		LOGGER.info("Ooh you already know who it is...");
+	}
+
+	public static @NotNull String getModName(Item item) {
+		String modName = "Unknown";
+
+		Optional<ModContainer> mod = FabricLoader.getInstance().getModContainer(Registries.ITEM.getId(item).getNamespace());
+		if (mod.isPresent()) {
+			modName = mod.get().getMetadata().getName();
+		}
+
+		return modName;
 	}
 }
