5fd10b51c9b36c68b02a6c5b1cf69f43a164e22e
Camper_Samu <samueleverga@gmail.com>
2024-10-03 21:37:53 +0200

    fix: prevent NPE crash related to the misuse of the LoreComponent
    Some mods might misuse this component, providing Text but not Styled Text.
    This fix adds an extra null-check and workaround for this.

--- src/main/java/com/campersamu/shoutout/mixin/AppendModNameToPacketBuffer.java
+++ src/main/java/com/campersamu/shoutout/mixin/AppendModNameToPacketBuffer.java
@@ -1,6 +1,7 @@
 package com.campersamu.shoutout.mixin;
 
 import com.campersamu.shoutout.duck.OriginalItemDuck;
+import com.google.common.collect.Lists;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.component.ComponentChanges;
 import net.minecraft.component.ComponentMapImpl;
@@ -9,7 +10,9 @@ import net.minecraft.component.type.LoreComponent;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.RegistryByteBuf;
 import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
+import net.minecraft.text.Texts;
 import net.minecraft.util.Formatting;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
@@ -28,6 +31,9 @@ import static net.minecraft.text.Text.literal;
 
 @Mixin(targets = "net/minecraft/item/ItemStack$1", priority = 5000)
 public class AppendModNameToPacketBuffer {
+    //fixme: workaround LoreComponent([lore], null) misuse
+    //       check LoreComponent#STYLE
+    @Unique private static final Style DEFAULT_LORE_STYLE = Style.EMPTY.withColor(Formatting.DARK_PURPLE).withItalic(true);
 
     @Redirect(method = "encode(Lnet/minecraft/network/RegistryByteBuf;Lnet/minecraft/item/ItemStack;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/codec/PacketCodec;encode(Ljava/lang/Object;Ljava/lang/Object;)V", ordinal = 1))
     private void shoutout$appendStuff(@NotNull PacketCodec<RegistryByteBuf, ComponentChanges> instance, Object _buf, Object _changes, @Local(argsOnly = true) ItemStack itemStack) {
@@ -68,11 +74,16 @@ public class AppendModNameToPacketBuffer {
         // Append mod name
         final var modText = literal(modName).formatted(Formatting.BLUE, Formatting.ITALIC);
 
+        //fixme: workaround LoreComponent([lore], null) misuse
+        final List<Text> styledLore = (lore.styledLines() != null)
+                ? lore.styledLines()
+                : Lists.transform(lore.lines(), style -> Texts.setStyleIfAbsent(style.copy(), DEFAULT_LORE_STYLE));
+
         // Check if the mod name is already present to avoid duplication (edge-case proofing)
-        if (!lore.styledLines().contains(modText))
+        if (!styledLore.contains(modText))
             lore = lore.with(modText); // Add the lore (record since 1.20.5, it returns an updated version of itself)
         else {
-            final var loreList = new ArrayList<>(lore.styledLines());
+            final var loreList = new ArrayList<>(styledLore);
             loreList.remove(modText);
             lore = new LoreComponent(loreList);
         }
